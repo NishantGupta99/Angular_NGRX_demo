10294d731d3ac7fc7671753102d6ad448873647e
ngactive99 <ngactive99@gmail.com>
2022-01-27 17:24:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
+import { GalleryService } from './gallery/gallery.service';
 import { AppComponent } from './app.component';
+import { galleryReducer } from './store/gallery.reducer';
+import { FormsModule } from '@angular/forms';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent, GalleryComponent],
   imports: [
-    BrowserModule
+    StoreModule.forRoot({gallery: galleryReducer}),
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [GalleryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
